a3d330f6cbe599b04b43dc7183bf37fa64f83c79
Rudra Pinamkar <rudrapinamkar88@gmail.com>
2026-08-15 21:36:13 +0530

    Update Frequency.cpp

--- Hashmaps/Frequency.cpp
+++ Hashmaps/Frequency.cpp
@@ -2,9 +2,11 @@
 #include<iostream>
 #include<string>
 #include<unordered_map>
+#include<stack>
 using namespace std;
 
-int main(){
+void frequency_counter()
+{
     string name = "rudra";     
     unordered_map<char ,int> mp;   // Here we are declaring the map with the kayy vlaue pari going to include char and not string as we are going couting the freqeucy of the char and nto that of the string
     for(char ch : name){ 
@@ -14,4 +16,26 @@ int main(){
     for(unordered_map<char,int>::iterator it = mp.begin() ; it!=mp.end(); ++it){   // Learn this syntax unordered_map<char, int>::iterator it = np.begin ; it!=mp.end() ;it++)
         cout<<it->first<<":"<<it->second<<endl;
     }
+}
+
+char majority_element(string st)// This fuction i snot including the two char havig similar frequecny
+{ 
+    
+    unordered_map<char ,int> map;
+    int res = 0;
+    char element;
+    for(char ch : st){
+        map[ch]++;
+        if(res<=map[ch]){
+            res=map[ch];
+            element =  ch;
+        }
+        
+    }
+    return element;
+}
+
+
+int main(){
+    
 }
