2e4a550538cb2da04a4ea54065607fd5e13bdc43
Rudra Pinamkar <rudrapinamkar88@gmail.com>
2026-08-15 10:24:10 +0530

    Update Theory.cpp

--- Dynamically_Allocated_Meomory/Theory.cpp
+++ Dynamically_Allocated_Meomory/Theory.cpp
@@ -5,3 +5,35 @@ char*ptr=new char[5];
 
 //But you might think that in ptr=new char , a new char was being created every time and the ptr was beign assigned to the latest assignment 
 //So in this in every loop ptr is being assigned to a new char , but in *ptr =new char[5] we are assiging the ptr to the whle array at one time , but still the ptr will only hold the adress of the first char 
+
+#About how to delete the array of instaneces from the heap 
+// This is really complex concept for me
+#include<iostream>
+#include <cstring>
+using namespace std;
+int main(){
+    const int max =256;
+    char buffer[max];
+    const int dim=3;
+    char*array[dim];
+    int index=0;
+    
+    while(index<dim&&cin.getline(buffer,max))// This while loop is important what it is doing is that , wehn the user inouts the string  the getline also reads the spces and the after entering teh string it is stored in buffer and the maximum limti being the max
+    {
+        if(index == dim){
+            cout<<"The array is full: ";
+        }
+        else{
+            char*ptr= new char[strlen(buffer) + 1];  // So here when teh user enter the string , we create location in the heap for the size for the given string
+            array[index++]=ptr;  // Then we are assigning the pointer from the pointer array to the adress were we just stored our input string to permanently save the input value
+            strcpy(ptr,buffer);
+        }
+        cout<<"Next String";
+    }
+    
+    cout<<"The strings are ";
+    for(int i =0 ;i<index;i++){
+        cout<<" "<<array[i];
+        delete array[i];
+    }
+}
