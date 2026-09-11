b64788f80455148b2aba89f656fc480eb82f73bb
Rudra Pinamkar <rudrapinamkar88@gmail.com>
2026-09-11 13:35:35 +0530

    Enhance comments on JavaScript and C++ comparison
    Added comments explaining JavaScript's dynamic nature compared to C++.

---
+++ Javascript/Basic.js
@@ -0,0 +1,12 @@
+//Using consle.log(value) to rpitn the value
+console.log(value) // is used to print
+//Like cpp here too heyhave const
+const value = 2;
+let -> is used to assign a variable
+Similarly var is used too
+
+#imp
+//Javascript is a dynamic language which is that the value of the varaible can be chanegd at run time
+//CPP is a static laguage althoguh the dynamic memory can be aces in the C
+// Although in some laguage they are comletely static - 
+This usually means that javascript is directly accesign heap memoery instad of static
